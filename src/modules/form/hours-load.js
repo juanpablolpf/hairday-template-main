b45b21f041418bdc90be4923b6d5b07496fd19a6
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-05-05 12:38:11 -0300

    selecting a time

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs"
 import {openingHours} from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
@@ -38,6 +39,9 @@ export function hoursLoad({date}){
         hours.append(li)
 
     })
+
+    //adicona o evente de click para os horarios disponives
+    hoursClick()
 }
 
 function hourHeaderAdd(title){
