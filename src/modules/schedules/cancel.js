bf80ff7333b58cffeb4463ac5d5a5a872cacc6db
Juan Pablo Filho <juan.pablo290102@gmail.com>
2025-05-07 16:06:54 -0300

    removing a schedule from the API

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,24 +1,34 @@
+import {schedulesDay} from "./load.js"
+import {scheduleCancel} from "../../services/schedule-cancel.js"
+
 const periods = document.querySelectorAll(".period")
 
 
 //gerar evento de click para cada periodo
 periods.forEach((period) => {
     //capturar o evento de click na lista
-    period.addEventListener("click", (event) => {
+    period.addEventListener("click", async (event) => {
     if(event.target.classList.contains("cancel-icon")){
         //obter a li pai do elemento clicado
         const item = event.target.closest("li")
+
+        //pega o id do agendametno para remover
         const { id } = item.dataset
         
 
-
+        //confirma que o id selecionado
         if(id){
+            //confirma que o usario quer cancelar
             const isConfirm = confirm(
                 "Tem certeza que deseja cancelar o agendamento?"
             )
 
-            if(confirm){
-                console.log("REMOVER!")
+            if(isConfirm){
+                //faz a requisição na API para cancelar
+                await scheduleCancel({id})
+
+                //recarrega os agendamentos
+                schedulesDay()
             }
         }
     }    
